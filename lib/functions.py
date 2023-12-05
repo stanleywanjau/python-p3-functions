74fcb570e1f65068be71ca26f05f814db0225126
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-05 05:37:30 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+     print("Hello, programmer!")
 
 def greet(name):
-    pass
+     print(f"Hello, {name}!")
+greet("Guido")
+     
+
 
 def greet_with_default(name="programmer"):
-    pass
-
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("James")
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+result = add(3, 5)
+print(result)
 
 def halve(number):
-    pass
+    return number/2
+results=halve(3)
+print(results)
